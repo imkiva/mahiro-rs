dee9c0d237232baeab81075be56bd357397db211
imkiva <libkernelpanic@gmail.com>
2020-04-14 19:27:19 +0800

    parse: trait ParseTo

--- core/src/parse.rs
+++ core/src/parse.rs
@@ -2,22 +2,47 @@ use crate::tree::*;
 
 use pest::Parser;
 use pest::iterators::{Pair, Pairs};
+use pest::error::Error;
+use pest::error::ErrorVariant;
+
+pub type ParseErrorVariant = ErrorVariant<Rule>;
+pub type ParseError = Error<Rule>;
+
+#[derive(Debug)]
+pub struct CompileError(pub ParseError);
+
+pub type CompileResult<T> = Result<T, CompileError>;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 pub struct CsParser;
 
-impl Into<Expr> for Pair<'_, Rule> {
-    fn into(self) -> Expr {
+trait ParseTo<T> {
+    fn parse_to(self) -> T;
+}
+
+impl ParseTo<Expr> for Pair<'_, Rule> {
+    fn parse_to(self) -> Expr {
         unimplemented!()
     }
 }
 
-impl Into<Stmt> for Pair<'_, Rule> {
-    fn into(self) -> Stmt {
+impl ParseTo<Stmt> for Pair<'_, Rule> {
+    fn parse_to(self) -> Stmt {
+        unimplemented!()
+    }
+}
+
+impl ParseTo<Program> for Pairs<'_, Rule> {
+    fn parse_to(self) -> Program {
         unimplemented!()
     }
 }
 
 impl CsParser {
+    pub fn ast(input: &str) -> CompileResult<Program> {
+        let parse = CsParser::parse(Rule::program, input);
+        let pairs = parse.map_err(|e| CompileError(e))?;
+        Ok(pairs.parse_to())
+    }
 }
--- core/src/tests.rs
+++ core/src/tests.rs
@@ -1,4 +1,4 @@
-#[cfg!(test)]
+#[cfg(test)]
 mod tests {
     #[test]
     fn works() {
