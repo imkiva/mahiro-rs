de48a945233ef18a8ce030110d709ba00aad84a6
imkiva <libkernelpanic@gmail.com>
2020-04-20 22:03:19 +0800

    compiler: compile to ast

--- compiler/src/capi.rs
+++ compiler/src/capi.rs
@@ -5,25 +5,24 @@ use libc::c_uchar;
 use libc::size_t;
 
 use crate::parse::{CsParser, CompileError};
-use crate::parse::CompileResult;
-use crate::parse::ParseError;
 
 #[repr(C)]
 pub enum CCompileResultKind {
     Success,
-    ParseError,
+    CompileError,
     MallocError,
+    InternalError,
 }
 
 #[repr(C)]
 pub struct CCompileResult {
     /// indicate compile result variant
-    error_occurred: CCompileResultKind,
+    kind: CCompileResultKind,
 
-    /// store the error message.
+    /// store the output message (nullable).
     /// this is malloc-ed, the caller is
     /// responsible to call free() on this.
-    error_message: *const c_char,
+    message: *const c_char,
 
     /// store the compiled bytecode.
     /// this is malloc-ed, the caller is
@@ -34,26 +33,27 @@ pub struct CCompileResult {
     compiled_code_length: size_t,
 }
 
+#[allow(dead_code)]
 impl CCompileResult {
-    pub fn from_error(msg: &str) -> Self {
-        let len = msg.len();
+    fn from_string(str: &str, kind: CCompileResultKind) -> Self {
+        let len = str.len();
         // '\0' takes 1 byte
         let buffer = malloc(len + 1);
 
         if buffer.is_null() {
             return CCompileResult {
-                error_occurred: CCompileResultKind::MallocError,
-                error_message: std::ptr::null(),
+                kind: CCompileResultKind::MallocError,
+                message: std::ptr::null(),
                 compiled_code: std::ptr::null(),
                 compiled_code_length: 0,
             };
         }
 
-        unsafe { std::ptr::copy(msg.as_ptr() as *const c_char, buffer, len) }
+        unsafe { std::ptr::copy(str.as_ptr() as *const c_char, buffer, len) }
 
         CCompileResult {
-            error_occurred: CCompileResultKind::ParseError,
-            error_message: buffer,
+            kind,
+            message: buffer,
             compiled_code: std::ptr::null(),
             compiled_code_length: 0,
         }
@@ -61,7 +61,8 @@ impl CCompileResult {
 
     pub fn from_compile_error(file: &str, err: CompileError) -> CCompileResult {
         let err = err.0;
-        CCompileResult::from_error(&format!("{}", err.with_path(file)))
+        CCompileResult::from_string(&format!("{}", err.with_path(file)),
+                                    CCompileResultKind::CompileError)
     }
 
     pub fn from_code(code: &[u8]) -> CCompileResult {
@@ -70,8 +71,8 @@ impl CCompileResult {
 
         if buffer.is_null() {
             return CCompileResult {
-                error_occurred: CCompileResultKind::MallocError,
-                error_message: std::ptr::null(),
+                kind: CCompileResultKind::MallocError,
+                message: std::ptr::null(),
                 compiled_code: std::ptr::null(),
                 compiled_code_length: 0,
             };
@@ -80,8 +81,8 @@ impl CCompileResult {
         unsafe { std::ptr::copy(code.as_ptr() as *const c_uchar, buffer, len) }
 
         CCompileResult {
-            error_occurred: CCompileResultKind::Success,
-            error_message: std::ptr::null(),
+            kind: CCompileResultKind::Success,
+            message: std::ptr::null(),
             compiled_code: buffer,
             compiled_code_length: len,
         }
@@ -103,14 +104,39 @@ fn to_rust_str<'a>(cstr: *const c_char) -> &'a str {
 #[no_mangle]
 pub extern "C" fn compile_source(file: *const c_char, src: *const c_char) -> CCompileResult {
     let result = catch_unwind(|| {
-        let file = to_rust_str(file);
-        let src = to_rust_str(src);
+        let _file = to_rust_str(file);
+        let _src = to_rust_str(src);
         // TODO: codegen
-        CCompileResult::from_error("unimplemented")
+        CCompileResult::from_string("unimplemented",
+                                    CCompileResultKind::InternalError)
     });
 
     match result {
         Ok(r) => r,
-        Err(e) => CCompileResult::from_error(&format!("{:?}", e)),
+        Err(e) => CCompileResult::from_string(
+            &format!("{:?}", e),
+            CCompileResultKind::InternalError),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn compile_to_ast(file: *const c_char, src: *const c_char) -> CCompileResult {
+    let result = catch_unwind(|| {
+        let file = to_rust_str(file);
+        let src = to_rust_str(src);
+        match CsParser::ast(src) {
+            Ok(tree) => CCompileResult::from_string(
+                &format!("{:#?}", tree),
+                CCompileResultKind::Success,
+            ),
+            Err(err) => CCompileResult::from_compile_error(file, err),
+        }
+    });
+
+    match result {
+        Ok(r) => r,
+        Err(e) => CCompileResult::from_string(
+            &format!("{:?}", e),
+            CCompileResultKind::InternalError),
     }
 }
