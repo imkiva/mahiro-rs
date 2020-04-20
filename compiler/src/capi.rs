db192f53f8ed2ee9f6d76991c91b6697b50ee8cd
imkiva <libkernelpanic@gmail.com>
2020-04-20 21:05:13 +0800

    compiler: capi

--- compiler/src/capi.rs
+++ compiler/src/capi.rs
@@ -1,4 +1,5 @@
 use std::panic::catch_unwind;
+use std::ffi::CStr;
 use libc::c_char;
 use libc::c_uchar;
 use libc::size_t;
@@ -7,16 +8,100 @@ use crate::parse::CsParser;
 use crate::parse::CompileResult;
 use crate::parse::ParseError;
 
+const ERROR_KIND_NO_ERROR: i32 = 0;
+const ERROR_KIND_PARSE: i32 = 1;
+const ERROR_KIND_MALLOC: i32 = 2;
+
 #[repr(C)]
-struct CCompileResult {
+pub struct CCompileResult {
+    /// indicate compile result variant
     error_occurred: i32,
+
+    /// store the error message.
+    /// this is malloc-ed, the caller is
+    /// responsible to call free() on this.
     error_message: *const c_char,
+
+    /// store the compiled bytecode.
+    /// this is malloc-ed, the caller is
+    /// responsible to call free() on this.
     compiled_code: *const c_uchar,
+
+    /// the size of the compiled code.
     compiled_code_length: size_t,
+}
+
+impl CCompileResult {
+    pub fn from_error(msg: &str) -> Self {
+        let len = msg.len();
+        // '\0' takes 1 byte
+        let buffer = malloc(len + 1);
+
+        if buffer.is_null() {
+            return CCompileResult {
+                error_occurred: ERROR_KIND_MALLOC,
+                error_message: std::ptr::null(),
+                compiled_code: std::ptr::null(),
+                compiled_code_length: 0,
+            };
+        }
+
+        unsafe { std::ptr::copy(msg.as_ptr() as *const c_char, buffer, len) }
+
+        CCompileResult {
+            error_occurred: ERROR_KIND_PARSE,
+            error_message: buffer,
+            compiled_code: std::ptr::null(),
+            compiled_code_length: 0,
+        }
+    }
+
+    pub fn from_code(code: &[u8]) -> CCompileResult {
+        let len = code.len();
+        let buffer = malloc(len) as *mut c_uchar;
+
+        if buffer.is_null() {
+            return CCompileResult {
+                error_occurred: ERROR_KIND_MALLOC,
+                error_message: std::ptr::null(),
+                compiled_code: std::ptr::null(),
+                compiled_code_length: 0,
+            };
+        }
+
+        unsafe { std::ptr::copy(code.as_ptr() as *const c_uchar, buffer, len) }
+
+        CCompileResult {
+            error_occurred: ERROR_KIND_NO_ERROR,
+            error_message: std::ptr::null(),
+            compiled_code: buffer,
+            compiled_code_length: len,
+        }
+    }
+}
+
+fn malloc(size: size_t) -> *mut c_char {
+    unsafe {
+        let buffer = libc::malloc(size);
+        libc::memset(buffer, 0, size);
+        buffer as *mut c_char
+    }
+}
+
+fn to_rust_str<'a>(cstr: *const c_char) -> &'a str {
+    unsafe { CStr::from_ptr(cstr) }.to_str().unwrap()
 }
 
 #[no_mangle]
 pub extern "C" fn compile_source(src: *const c_char) -> CCompileResult {
     let result = catch_unwind(|| {
+        let str = to_rust_str(src);
+        // TODO: codegen
+        CCompileResult::from_error("unimplemented")
     });
+
+    match result {
+        Ok(r) => r,
+        Err(e) => CCompileResult::from_error(&format!("{:?}", e)),
+    }
 }
