205b1990ff19672d0ed4920bb2eb5d0b41b3cb98
imkiva <libkernelpanic@gmail.com>
2020-04-21 21:56:31 +0800

    pe: prepare for pe

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -2,12 +2,7 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-pub mod pe;
 pub mod error;
-
 pub mod syntax;
 
 mod capi;
-
-#[cfg(test)]
-mod tests;
--- compiler/src/syntax/mod.rs
+++ compiler/src/syntax/mod.rs
@@ -1,3 +1,7 @@
 pub mod parse;
 pub mod tree;
 pub mod desugar;
+pub mod pe;
+
+#[cfg(test)]
+mod tests;
