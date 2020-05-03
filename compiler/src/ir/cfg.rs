3fe7d6e16640720a414c5995361f37d906e69ed4
imkiva <libkernelpanic@gmail.com>
2020-05-03 22:38:13 +0800

    cfg: masm

--- compiler/src/ir/cfg.rs
+++ compiler/src/ir/cfg.rs
@@ -3,7 +3,7 @@ use crate::syntax::tree::Program;
 
 pub type BBUnderlyingID = u32;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub enum BBID {
     This,
     That(BBUnderlyingID),
@@ -11,8 +11,10 @@ pub enum BBID {
 
 #[derive(Debug, Clone)]
 pub struct BasicBlock {
-    pub id: BBUnderlyingID,
     pub ir: Vec<IR>,
+    pub id: BBUnderlyingID,
+    pub succ: BBID,
+    pub pred: BBID,
 }
 
 #[derive(Debug, Clone)]
@@ -33,3 +35,40 @@ impl CodeUnit {
         Self::new()
     }
 }
+
+struct MacroAssembler {
+    editing: BasicBlock,
+}
+
+impl MacroAssembler {
+    pub fn new() -> Self {
+        Self {
+            editing: BasicBlock {
+                ir: Vec::new(),
+                id: 0,
+                succ: BBID::This,
+                pred: BBID::This
+            }
+        }
+    }
+
+    pub fn with_id(mut self, id: BBUnderlyingID) -> Self {
+        self.editing.id = id;
+        self
+    }
+
+    pub fn with_pred(mut self, block: BBID) -> Self {
+        self.editing.pred = block;
+        self
+    }
+
+    pub fn with_succ(mut self, block: BBID) -> Self {
+        self.editing.succ = block;
+        self
+    }
+
+    pub fn emit(&mut self, ir: IR) {
+        self.editing.ir.push(ir);
+    }
+}
+
