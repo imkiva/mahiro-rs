82ef81f51a068b79452e46ccce4275bc05d2cf8b
imkiva <libkernelpanic@gmail.com>
2020-10-11 23:33:11 +0800

    fix: grammar: enum variant comma

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use mahiro_lang::syntax::parser::ModuleParser;
 fn main() {
     let m = ModuleParser::new().parse(r#"
     module Main where
+    enum Option<T> {
+        Some(T),
+        None,
+    }
+    trait Hello {
+        fn say();
+        fn nice(a: UInt32) -> Bool;
+    }
     fn main() {
         println("hello");
     }
