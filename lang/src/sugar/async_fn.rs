cb696cdac42ea91dd1ebef95f1ee9a250797fa30
imkiva <libkernelpanic@gmail.com>
2020-10-15 12:21:33 +0800

    grammar: desugar async when parsing

--- lang/src/sugar/async_fn.rs
+++ lang/src/sugar/async_fn.rs
@@ -3,7 +3,7 @@ use crate::syntax::tree::{Decl, FnSig, Stmt};
 pub struct DesugarAsync;
 
 impl DesugarAsync {
-  pub fn desugar_async(sig: &FnSig, body: Vec<Stmt>) -> (Vec<Stmt>, Vec<Decl>) {
+  pub fn desugar_async(_: &FnSig, body: Vec<Stmt>) -> (Vec<Stmt>, Vec<Decl>) {
     (body, vec![])
   }
 }
