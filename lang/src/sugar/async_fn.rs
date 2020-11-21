c2069a105ae6aab28a2b7dfd5c1fd378e1fba37f
imkiva <libkernelpanic@gmail.com>
2020-11-21 12:07:57 +0800

    async/await: not supported yet

--- lang/src/sugar/async_fn.rs
+++ lang/src/sugar/async_fn.rs
@@ -3,7 +3,7 @@ use crate::syntax::tree::{Decl, FnSig, Stmt, Expr, Param};
 pub struct DesugarAsync;
 
 impl DesugarAsync {
-  pub fn desugar_async(sig: &FnSig, body: Vec<Stmt>) -> (Vec<Stmt>, Vec<Decl>) {
-    (body, vec![])
+  pub fn desugar_async(_: &FnSig, _: Vec<Stmt>) -> (Vec<Stmt>, Vec<Decl>) {
+    unimplemented!("async/await is not supported yet")
   }
 }
